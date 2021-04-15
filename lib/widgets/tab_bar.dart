93cd3b6a4861b5cb4077b59b85e675c7b4bbef28
CinqueIzumi <nofeednolife@gmail.com>
2021-04-15 22:32:25 +0200

    TabBar is now interactable

--- lib/widgets/tab_bar.dart
+++ lib/widgets/tab_bar.dart
@@ -5,42 +5,56 @@ class CustomTabBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 4,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 50,
-            child: AppBar(
-              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-              elevation: 0.0,
-              bottom: TabBar(
-                indicatorColor: Colors.grey,
-                tabs: [
-                  Tab(
-                    icon: Icon(
-                      Icons.grid_on_outlined,
-                      color: Colors.grey,
-                    ),
+      child: Expanded(
+        child: Scaffold(
+          body: Column(
+            children: [
+              SizedBox(
+                height: 50,
+                child: AppBar(
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  elevation: 0.0,
+                  bottom: TabBar(
+                    indicatorColor: Colors.grey,
+                    tabs: [
+                      Tab(
+                        icon: Icon(
+                          Icons.grid_on_outlined,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Tab(
+                        icon: Icon(
+                          Icons.ondemand_video_sharp,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Tab(
+                        icon: Icon(Icons.tv, color: Colors.grey),
+                      ),
+                      Tab(
+                        icon: Icon(
+                          Icons.account_box_outlined,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
                   ),
-                  Tab(
-                    icon: Icon(
-                      Icons.ondemand_video_sharp,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  Tab(
-                    icon: Icon(Icons.tv, color: Colors.grey),
-                  ),
-                  Tab(
-                    icon: Icon(
-                      Icons.account_box_outlined,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Center(child: Text("First Tab")),
+                    Center(child: Text("Second Tab")),
+                    Center(child: Text("Third Tab")),
+                    Center(child: Text("Fourth Tab")),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
